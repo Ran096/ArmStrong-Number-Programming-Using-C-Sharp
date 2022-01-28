0e29f440bc41f959d63d0c92553e9b15d946cda8
ran096 <ranjeetpatel096@gmail.com>
2022-01-28 18:17:41 +0530

    A Generic Class with Two Type Parameters With Programming

--- ArmStrong Number Programming Using C Sharp/Program.cs
+++ ArmStrong Number Programming Using C Sharp/Program.cs
@@ -140,7 +140,7 @@ namespace GenClassDemo
     }
 
 }
- */
+*/
 using System;
 public class Program
 {
@@ -150,13 +150,14 @@ public class Program
     {
         int digit;
         int cnt = 0, Start, end;
+        Console.WriteLine("Enter Digit ");
+        digit = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine(" Armstrong starting Number Enter");
         Start = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Armstrong Ending Number Enter");
         end = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Armstrong Number from starting :" + Start + " to Ending: " + end);
-        Console.WriteLine("Enter Digit ");
-        digit = Convert.ToInt32(Console.ReadLine());
+       
 
         for (int m = Start; m <= end; m++)
         {
@@ -178,3 +179,100 @@ public class Program
     }
 }
 
+    /*
+// A simple generic class with two type parameters: T and V.
+using System;
+namespace GenDemo
+{
+    class Gen<T, V, K>
+    {
+        T obj1;
+        V obj2;
+        K obj3;
+
+        public Gen(T o1, V o2 , K o3)
+        {
+            obj1 = o1;
+            obj2 = o2;
+            obj3 = o3;
+        }
+        public void showTypes()
+        {
+            Console.WriteLine("Type of T is " + typeof(T));
+            Console.WriteLine("Type of V is " + typeof(V));
+            Console.WriteLine("Type of V is " + typeof(K));
+        }
+        public T get1()
+        {
+            return obj1;
+        }
+        public V get2()
+        {
+            return obj2;
+        }
+        public K get3()
+        {
+            return obj3;
+        }
+    }
+    class GenDemo
+    {
+        public static void Main()
+        {
+            Gen<int, string, double> obstr = new Gen<int, string, double>(100, "Add New Power String ", 10.5);
+            obstr.showTypes();
+            int v = obstr.get1();
+            Console.WriteLine("value: " + v);
+            string str = obstr.get2();
+            Console.WriteLine("value:" + str);
+            double f = obstr.get3();
+            Console.WriteLine("value:" + f);
+        }
+    }
+}
+
+// A simple demonstration of a base class constraint.
+using System;
+class A
+{
+    public void Hello()
+    {
+        Console.WriteLine("Hello");
+    }
+}
+
+class B : A { }
+
+class C : A { }
+
+class Test<T> where T : A
+{
+    T obj;
+    public Test(T o)
+    {
+        obj = o;
+    }
+    public void SayHello()
+    {
+       
+        obj.Hello();
+    }
+}
+class BaseClassConstraintDemo
+{
+    static void Main()
+    {
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        Test<A> t1 = new Test<A>(a);
+        t1.SayHello();
+
+        Test<B> t2 = new Test<B>(b);
+        t2.SayHello();
+        Test<C>t=new Test<C>(c);
+        t.SayHello();
+    }
+}
+*/
